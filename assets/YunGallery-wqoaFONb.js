5a9cfd6058bd145970c56389dcfb97f2b0c37c82
MengNianxiaoyao <2589141604@qq.com>
2024-06-15 11:31:23 +0000

    update

--- assets/YunGallery-wqoaFONb.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./gallery-DcVmNeJf.js";import"./app-BfU0qhUM.js";import"./YunPageHeader.vue_vue_type_script_setup_true_lang-DVJY2cIe.js";export{o as default};
---
+++ assets/back-D-ONbtQT.js
@@ -0,0 +1,1 @@
+import{u as o}from"./app-COSmWvn6.js";function t(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{t as u};
