a642581e63bd3faa4847d5c6a8ee798412eedac6
MengNianxiaoyao <2589141604@qq.com>
2024-08-02 04:26:11 +0000

    chore: vercel增加缓存设置

--- assets/YunGallery-D34CnmbW.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./gallery-BpsptOCO.js";import"./app-Dq4j6VHg.js";import"./YunPageHeader.vue_vue_type_script_setup_true_lang-DHLXyylH.js";export{o as default};
---
+++ assets/YunGallery-DCIW3JwS.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./gallery-CDrI5CBO.js";import"./app-DLIa8zvd.js";import"./YunPageHeader.vue_vue_type_script_setup_true_lang-DffztLfD.js";export{o as default};
