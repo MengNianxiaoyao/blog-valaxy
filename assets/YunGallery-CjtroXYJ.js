1e122a7fff9bd36144156bfc6faed074c18197a2
MengNianxiaoyao <2589141604@qq.com>
2024-08-02 09:47:39 +0000

    fix: 替换vercount为国内节点

--- assets/YunGallery-CjtroXYJ.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./gallery-Booz4RU4.js";import"./app-BIKfG67p.js";import"./YunPageHeader.vue_vue_type_script_setup_true_lang-C8G-bev-.js";export{o as default};
---
+++ assets/YunGallery-D-wfka8M.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./gallery-DJU5huu8.js";import"./app-CwIEJZAi.js";import"./YunPageHeader.vue_vue_type_script_setup_true_lang-DYMJlXUS.js";export{o as default};
