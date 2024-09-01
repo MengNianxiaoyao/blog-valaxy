34ff8c5533b103917d97049f2d5815a187b2e25f
MengNianxiaoyao <MengNianxiaoyao@users.noreply.github.com>
2024-09-01 21:03:51 +0000

    chore: upgrade deps

--- assets/YunGallery-BIjMc713.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./gallery-BMycea77.js";import"./app-HDXoRVwJ.js";import"./YunPageHeader.vue_vue_type_script_setup_true_lang-BdBw5pzP.js";export{o as default};
---
+++ assets/back-Ba1ciM-2.js
@@ -0,0 +1,1 @@
+import{u as o}from"./app-m_W9dXP7.js";function t(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{t as u};
