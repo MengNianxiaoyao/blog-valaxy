4757ec3e73d32819fc6e12a3c87cf332f1586b1b
MengNianxiaoyao <MengNianxiaoyao@users.noreply.github.com>
2025-01-23 21:04:33 +0000

    chore: upgrade deps

---
+++ assets/post.CFiinxu9.js
@@ -0,0 +1,1 @@
+import"./chunks/@vueuse/motion.ookUZRBl.js";import{o as i}from"./app.DpzEH9nj.js";import{B as t}from"./framework.DVAw3X3I.js";function l(o){if(!o)return{color:"",icon:"",styles:void 0};const n=i();o in n.value.types||(o="link");const r=n.value.types[o].color,e=n.value.types[o].icon,s=t(()=>o?{"--card-c-primary":r}:void 0);return{color:r,icon:e,styles:s}}export{l as u};
