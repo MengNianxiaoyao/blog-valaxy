42494958e2c74b5a57037ddf7abb80af1759b5bc
MengNianxiaoyao <MengNianxiaoyao@users.noreply.github.com>
2025-04-10 21:09:28 +0000

    chore: 优化缓存策略

--- assets/back.DclnQ0V4.js
+++
@@ -1,1 +0,0 @@
-import{u as o}from"./chunks/vue-router.B9xvw7L7.js";function t(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{t as u};
