fd1a7badf6b6e58649e4d448162011e2e4202ac3
MengNianxiaoyao <2589141604@qq.com>
2025-04-11 08:30:54 +0000

    feat

---
+++ assets/post.CnKbSDX_.js
@@ -0,0 +1,1 @@
+import"./chunks/@vueuse/motion.BltUbIEt.js";import{o as t}from"./app.CMNYEJM8.js";import{A as e}from"./framework.Wgu5ILjI.js";function m(o){if(!o)return{color:"",icon:"",styles:void 0};const r=t();o in r.value.types||(o="link");const s=r.value.types[o].color,i=r.value.types[o].icon,n=e(()=>o?{"--card-c-primary":s}:void 0);return{color:s,icon:i,styles:n}}export{m as u};
