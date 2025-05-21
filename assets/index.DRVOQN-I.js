22215c3f2b7c78bb1a84ec48e95d29fc76cf31d9
MengNianxiaoyao <2589141604@qq.com>
2025-05-21 14:27:40 +0000

    优化性能

---
+++ assets/index.DRVOQN-I.js
@@ -0,0 +1,1 @@
+import"./chunks/@vueuse/motion.DxIu6Xbo.js";import{A as s}from"./app.B0nKYDg3.js";import"./chunks/dayjs.Byk5cVHE.js";import"./animation.DwG1iAC7.js";const m="/assets/none.DLM1AMsD.jpg";function p(t,o=m){const r=t.target;r.setAttribute("data-src",r.src),r.src=o}function c(t){return s(t,{template:"YYYY-MM-DD HH:mm:ss"})}export{c as f,p as o};
