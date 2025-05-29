e280a9080f8a56106a694f197d51586cc0c56e42
MengNianxiaoyao <2589141604@qq.com>
2025-05-29 05:08:51 +0000

    修改鼠标样式

--- assets/back.DcR5MZX3.js
+++
@@ -1,1 +0,0 @@
-import{u as o}from"./chunks/vue-router.CA_qz6A7.js";function t(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{t as u};
