c302d725f0b62d397d7d04ff57b21fd5f0e60ba2
MengNianxiaoyao <2589141604@qq.com>
2025-05-31 14:50:52 +0000

    更新

--- assets/back.KapF9NkC.js
+++
@@ -1,1 +0,0 @@
-import{u as o}from"./chunks/vue-router.Btt3c5Bt.js";function t(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{t as u};
