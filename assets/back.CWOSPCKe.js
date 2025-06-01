725d4cc7434fe4625adc829d355a98c2cf381ec9
MengNianxiaoyao <2589141604@qq.com>
2025-06-01 08:17:46 +0000

    更新缓存配置

--- assets/back.CWOSPCKe.js
+++
@@ -1,1 +0,0 @@
-import{u as o}from"./chunks/vue-router.BVJXUyB9.js";function t(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{t as u};
