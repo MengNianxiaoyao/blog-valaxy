3fec211a2402451a61811dceaf8cd94a2147f3fc
MengNianxiaoyao <2589141604@qq.com>
2025-06-01 09:32:01 +0000

    更新

---
+++ assets/back.DIMn5_ut.js
@@ -0,0 +1,1 @@
+import{u as o}from"./chunks/vue-router.C7mE1rpL.js";function t(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{t as u};
