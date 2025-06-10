c06dadfc404e204505b1e06463178152197c22c0
MengNianxiaoyao <MengNianxiaoyao@users.noreply.github.com>
2025-06-10 21:10:10 +0000

    saas

--- assets/post.CcZo3TQT.js
+++
@@ -1,1 +0,0 @@
-import"./chunks/@vueuse/motion.C0trivID.js";import{o as t}from"./app.oXTcDwqr.js";import{M as e}from"./framework.DAJS44PW.js";function m(o){if(!o)return{color:"",icon:"",styles:void 0};const r=t();o in r.value.types||(o="link");const s=r.value.types[o].color,i=r.value.types[o].icon,n=e(()=>o?{"--card-c-primary":s}:void 0);return{color:s,icon:i,styles:n}}export{m as u};
