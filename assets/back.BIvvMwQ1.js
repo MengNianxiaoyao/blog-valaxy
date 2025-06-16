e1eae9cb0aa7fa7b175f1488a6caa9331d27b2f4
MengNianxiaoyao <2589141604@qq.com>
2025-06-16 13:23:37 +0000

    Merge branch 'main' of https://github.com/MengNianxiaoyao/blog

--- assets/back.BIvvMwQ1.js
+++
@@ -1,1 +0,0 @@
-import{u as o}from"./chunks/vue-router.C5Cv8tDc.js";function t(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{t as u};
