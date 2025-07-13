ba7103e98e207f6b0b6f9375690b2ce23ddf5ab3
MengNianxiaoyao <2589141604@qq.com>
2025-07-13 01:16:58 +0000

    更新

--- assets/post.BFTK0_2M.js
+++
@@ -1,1 +0,0 @@
-import"./chunks/@vueuse/motion.CdzUAA8A.js";import{o as t}from"./app.DEpzR4vs.js";import{M as e}from"./framework.DDkCll1F.js";function m(o){if(!o)return{color:"",icon:"",styles:void 0};const r=t();o in r.value.types||(o="link");const s=r.value.types[o].color,i=r.value.types[o].icon,n=e(()=>o?{"--card-c-primary":s}:void 0);return{color:s,icon:i,styles:n}}export{m as u};
