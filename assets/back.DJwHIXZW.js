a11843d5811e788510f15df1a626cd55add190d6
MengNianxiaoyao <MengNianxiaoyao@users.noreply.github.com>
2025-07-15 21:08:50 +0000

    更新

---
+++ assets/back.DJwHIXZW.js
@@ -0,0 +1,1 @@
+import{u as o}from"./chunks/vue-router.ZTf0l2wC.js";function t(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{t as u};
