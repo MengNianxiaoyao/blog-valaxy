daf25762b437c8268af21d27801d266b5d9fc56a
MengNianxiaoyao <MengNianxiaoyao@users.noreply.github.com>
2025-08-07 21:08:59 +0000

    chore: upgrade deps

---
+++ assets/back.DEwMcccA.js
@@ -0,0 +1,1 @@
+import{a as o}from"./chunks/vue-router.D24eYAlW.js";function u(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{u};
