d1928ec98f1fce54ba92323193ba79544ff7f631
MengNianxiaoyao <2589141604@qq.com>
2025-08-08 11:58:14 +0000

    更新

--- assets/back.DEwMcccA.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./chunks/vue-router.D24eYAlW.js";function u(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{u};
