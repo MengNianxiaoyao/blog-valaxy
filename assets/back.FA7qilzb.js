013e90a1681af62b5e891fad657247c0cc8724c8
MengNianxiaoyao <MengNianxiaoyao@users.noreply.github.com>
2025-08-12 21:09:55 +0000

    更新

---
+++ assets/back.FA7qilzb.js
@@ -0,0 +1,1 @@
+import{a as o}from"./chunks/vue-router.Dp89X2xV.js";function u(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{u};
