2c624942b4acb689384f59109ca917d496e4bb66
MengNianxiaoyao <2589141604@qq.com>
2025-08-14 04:03:27 +0000

    修改页脚

--- assets/back.D0ISLUPI.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./chunks/vue-router.D5XR9Zdm.js";function u(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{u};
