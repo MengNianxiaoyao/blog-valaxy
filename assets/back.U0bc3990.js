b9e1307f61405ecf6156c82f3834ddd8a3bbec8e
MengNianxiaoyao <2589141604@qq.com>
2025-08-16 13:14:54 +0000

    添加徽标

---
+++ assets/back.U0bc3990.js
@@ -0,0 +1,1 @@
+import{a as o}from"./chunks/vue-router.B0qLb41L.js";function u(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{u};
