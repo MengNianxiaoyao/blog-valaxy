e3b4a88bb8762eac9dde9449de2ab9276f30ef9f
MengNianxiaoyao <2589141604@qq.com>
2025-08-26 14:16:01 +0000

    更新

--- assets/collections.b5QsmAEv.js
+++
@@ -1,1 +0,0 @@
-import"./chunks/@vueuse/motion.pHCYXPZe.js";import{u as l}from"./chunks/vue-router.ByF0Mflb.js";import{M as t}from"./framework.DIpn_G-j.js";const e=[];function a(){return{collections:t(()=>e)}}function p(){const o=l(),n=t(()=>o.path.startsWith("/collections/")?o.path.split("/")[2]:"");return{collection:t(()=>e.find(c=>c.key===n.value))}}export{a,p as u};
