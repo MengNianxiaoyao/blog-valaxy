d2c60c5a7c84a616e8cd64a99130b6ad0602eabd
MengNianxiaoyao <2589141604@qq.com>
2025-08-27 14:45:12 +0000

    测试

---
+++ assets/index.CriUbpL4.js
@@ -0,0 +1,1 @@
+import"./chunks/@vueuse/motion.CdQT0bqJ.js";import{D as s}from"./app.Drs5lhEk.js";import"./chunks/dayjs.BdcnXKr1.js";import"./animation.Bld2ewst.js";const m="/assets/none.DLM1AMsD.jpg";function p(t,o=m){const r=t.target;r.setAttribute("data-src",r.src),r.src=o}function c(t){return s(t,{template:"YYYY-MM-DD HH:mm:ss"})}export{c as f,p as o};
--- assets/post.Dnd5wPYj.js
+++
@@ -1,1 +0,0 @@
-import"./chunks/@vueuse/motion.pHCYXPZe.js";import{a8 as o,a9 as t,aa as r,ab as a}from"./framework.DIpn_G-j.js";const c={};function n(s,_){const e=t("RouterView");return a(),r(e)}const i=o(c,[["render",n]]);export{i as default};
