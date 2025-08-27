d8dad92fe6958408f9cb58e5e4aafa2277781680
MengNianxiaoyao <2589141604@qq.com>
2025-08-27 11:04:41 +0000

    更新徽标

---
+++ assets/post.CsPKxmu_.js
@@ -0,0 +1,1 @@
+import"./chunks/@vueuse/motion.pHCYXPZe.js";import{p as t}from"./app.CInx2qge.js";import{M as e}from"./framework.DIpn_G-j.js";function m(o){if(!o)return{color:"",icon:"",styles:void 0};const r=t();o in r.value.types||(o="link");const s=r.value.types[o].color,i=r.value.types[o].icon,n=e(()=>o?{"--card-c-primary":s}:void 0);return{color:s,icon:i,styles:n}}export{m as u};
